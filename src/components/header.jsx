453feecc53211f32ea9685db6de33fc190aded9f
ata_turkoglu <ata.turkoglu@reengen.com>
2024-01-08 18:15:49 +0300

    add new styles and font

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,40 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "./header.scss";
 import logo from "../assets/eile-logo.png";
+import roundLogo from "../assets/eile-round.png";
 import { Link } from "react-router-dom";
 
 function Header() {
+    const [narrowHeader, setNarrowHeader] = useState(false);
+    useEffect(() => {
+        function setHeaderHeight() {
+            if (window.scrollY > 100) {
+                setNarrowHeader(true);
+            } else {
+                setNarrowHeader(false);
+            }
+        }
+        window.addEventListener("scroll", setHeaderHeight);
+        return () => {
+            window.removeEventListener("scroll", setHeaderHeight);
+        };
+    }, []);
+
     return (
-        <div className="header">
-            <div className="logo-container">
-                <img src={logo} />
+        <div
+            className="header"
+            style={{
+                height: narrowHeader ? "50px" : "100px",
+                paddingBlock: narrowHeader ? "0" : "1rem",
+            }}
+        >
+            <div
+                className={
+                    narrowHeader ? "logo-container-narrow" : "logo-container"
+                }
+            >
+                <img src={narrowHeader ? roundLogo : logo} />
             </div>
             <div className="nav-container">
                 <Link className="nav-item" to="/">
