5d70d383caae97c3094e0f94e763d6be262a390f
ata_turkoglu <ata.turkoglu@reengen.com>
2024-01-08 23:31:31 +0300

    add new feature on header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,9 +4,13 @@ import "./header.scss";
 import logo from "../assets/eile-logo.png";
 import roundLogo from "../assets/eile-round.png";
 import { Link } from "react-router-dom";
+import productsJSON from "../data/products.json";
 
 function Header() {
     const [narrowHeader, setNarrowHeader] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [hoverProducts, setHoverProducts] = useState(false);
+
     useEffect(() => {
         function setHeaderHeight() {
             if (window.scrollY > 200) {
@@ -16,6 +20,11 @@ function Header() {
             }
         }
         window.addEventListener("scroll", setHeaderHeight);
+
+        const res = productsJSON.map((item) => {
+            return <p key={item.key}>{item.name}</p>;
+        });
+        setProducts(res);
         return () => {
             window.removeEventListener("scroll", setHeaderHeight);
         };
@@ -26,36 +35,68 @@ function Header() {
             className="header"
             style={{
                 height: narrowHeader ? "50px" : "100px",
-                paddingBlock: narrowHeader ? "0" : "1rem",
             }}
         >
-            <div
+            <Link
                 className={
                     narrowHeader ? "logo-container-narrow" : "logo-container"
                 }
+                to="/"
+                style={{ paddingBlock: narrowHeader ? "0" : "1rem" }}
             >
                 <img src={narrowHeader ? roundLogo : logo} />
-            </div>
+            </Link>
             <div
                 className="nav-container"
-                style={{ alignItems: narrowHeader ? "center" : "flex-end" }}
+                style={{
+                    alignItems: narrowHeader ? "center" : "flex-end",
+                }}
             >
-                <Link className="nav-item" to="/">
-                    Anasayfa
-                </Link>
-                <Link className="nav-item" to="/about">
-                    Hakkımızda
-                </Link>
-                <Link className="nav-item" to="/products">
+                <Link
+                    className="nav-item"
+                    to="/products"
+                    style={{
+                        paddingBlock: narrowHeader ? "0" : "1rem",
+                        backgroundColor: hoverProducts
+                            ? "rgb(245, 245, 245)"
+                            : "",
+                    }}
+                    onMouseOver={() => setHoverProducts(true)}
+                    onMouseLeave={() => setHoverProducts(false)}
+                >
                     Ürünler
                 </Link>
-                <Link className="nav-item" to="/docs">
+                <Link
+                    className="nav-item"
+                    to="/about"
+                    style={{ paddingBlock: narrowHeader ? "0" : "1rem" }}
+                >
+                    Hakkımızda
+                </Link>
+                <Link
+                    className="nav-item"
+                    to="/docs"
+                    style={{ paddingBlock: narrowHeader ? "0" : "1rem" }}
+                >
                     Belgelerimiz
                 </Link>
-                <Link className="nav-item" to="/references">
+                <Link
+                    className="nav-item"
+                    to="/references"
+                    style={{ paddingBlock: narrowHeader ? "0" : "1rem" }}
+                >
                     Referanslar
                 </Link>
             </div>
+            {hoverProducts && (
+                <div
+                    className="headerBelowPart"
+                    onMouseOver={() => setHoverProducts(true)}
+                    onMouseLeave={() => setHoverProducts(false)}
+                >
+                    {products}
+                </div>
+            )}
         </div>
     );
 }
