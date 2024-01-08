dc1cf4033f753922615e0504d7cd7218cdb507ac
fatmaaydogdu <fatmaaydogdu581@gmail.com>
2024-01-08 14:00:31 +0300

    navbar responsive

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.scss";
 import logo from "../assets/eile-logo.png";
-import { Link } from "react-router-dom";
+import { Link, NavLink} from "react-router-dom";
 
 function Header() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleActiveClass = () => {
+        setMenuOpen(!menuOpen);
+      };
+
+      const removeActive = () => {
+        setMenuOpen(false)
+      }
+
     return (
-        <div className="header">
+        <nav>
             <div className="logo-container">
                 <img src={logo} />
             </div>
-            <div className="nav-container">
-                <Link className="nav-item" to="/">
-                    Anasayfa
-                </Link>
-                <Link className="nav-item" to="/about">
-                    Hakkımızda
-                </Link>
-                <Link className="nav-item" to="/products">
-                    Ürünler
-                </Link>
-                <Link className="nav-item" to="/references">
-                    Referanslar
-                </Link>
+            <div className="menu" onClick={toggleActiveClass}>
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
-        </div>
+            <div>
+                
+            </div>
+            <ul className={menuOpen ? "open" : ""}>
+                <li onClick={removeActive}>
+                    <NavLink to="/">Anasayfa</NavLink>
+                </li>
+                <li onClick={removeActive}>
+                    <NavLink to="/about">Hakkımızda</NavLink>
+                </li>
+                <li onClick={removeActive}>
+                    <NavLink to="/products">Ürünler</NavLink>
+                </li>
+                <li onClick={removeActive}>
+                    <NavLink to="/references">Referanslar</NavLink>
+                </li>
+            </ul>
+        </nav>
+
     );
 }
+
+
 
 export default Header;
