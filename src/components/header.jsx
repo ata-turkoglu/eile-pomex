1e8d7fd8f3de4c449ebfc6d72c9dd696c8ab50b1
ata_turkoglu <ata.turkoglu@reengen.com>
2024-01-13 12:12:11 +0300

    refactor header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ import logo from "../assets/eile-logo.png";
 import roundLogo from "../assets/eile-round.png";
 import { Link } from "react-router-dom";
 import productsJSON from "../data/products.json";
-import { Instagram, Facebook, Twitter, Linkedin } from "lucide-react";
+import { Instagram, Facebook, Twitter, Linkedin, Phone } from "lucide-react";
 
 function Header() {
     const [narrowHeader, setNarrowHeader] = useState(false);
@@ -54,67 +54,133 @@ function Header() {
                     alignItems: narrowHeader ? "center" : "flex-end",
                 }}
             >
-                <Link
-                    className="nav-item"
-                    to="/products"
+                {!narrowHeader && (
+                    <div className="nav-container-top">
+                        <span
+                            style={{ marginRight: "3rem", fontSize: ".9rem" }}
+                        >
+                            Ankara Asfaltı Belkahve Mevkii No:277 Kavaklıdere
+                            Köyü Bornova / İZMİR
+                        </span>
+                        <Phone color="rgb(0,91,168)" size={18} />
+                        <span style={{ marginInline: "1rem" }}>
+                            0 232 360 17 77
+                        </span>
+                        <Linkedin
+                            color="rgb(0,91,168)"
+                            style={{
+                                cursor: "pointer",
+                                marginRight: "10px",
+                                marginLeft: "24px",
+                            }}
+                            size={18}
+                        />
+                        <Twitter
+                            color="rgb(0,91,168)"
+                            style={{ cursor: "pointer", marginRight: "10px" }}
+                            size={18}
+                        />
+                        <Instagram
+                            color="rgb(0,91,168)"
+                            style={{ cursor: "pointer", marginRight: "10px" }}
+                            size={18}
+                        />
+                        <Facebook
+                            color="rgb(0,91,168)"
+                            style={{ cursor: "pointer", marginRight: "10px" }}
+                            size={18}
+                        />
+                    </div>
+                )}
+                <div
+                    className="nav-container-bottom"
                     style={{
-                        paddingBlock: narrowHeader ? "0" : "1rem",
-                        alignItems: narrowHeader ? "center" : "flex-end",
-                    }}
-                    onMouseOver={() => setHoverProducts(true)}
-                    onMouseLeave={() => setHoverProducts(false)}
-                >
-                    Ürünler
-                </Link>
-                <Link
-                    className="nav-item"
-                    to="/about"
-                    style={{
-                        paddingBlock: narrowHeader ? "0" : "1rem",
-                        alignItems: narrowHeader ? "center" : "flex-end",
-                    }}
-                >
-                    Hakkımızda
-                </Link>
-                <Link
-                    className="nav-item"
-                    to="/docs"
-                    style={{
-                        paddingBlock: narrowHeader ? "0" : "1rem",
-                        alignItems: narrowHeader ? "center" : "flex-end",
-                    }}
-                >
-                    Belgelerimiz
-                </Link>
-                <Link
-                    className="nav-item"
-                    to="/references"
-                    style={{
-                        paddingBlock: narrowHeader ? "0" : "1rem",
-                        alignItems: narrowHeader ? "center" : "flex-end",
-                    }}
-                >
-                    Referanslar
-                </Link>
-                <Link
-                    className="nav-item"
-                    to="/contact"
-                    style={{
-                        paddingBlock: narrowHeader ? "0" : "1rem",
-                        alignItems: narrowHeader ? "center" : "flex-end",
+                        height: narrowHeader ? "100%" : "70%",
                     }}
                 >
-                    İletişim
-                </Link>
-            </div>
-            <div className="socialContainer">
-                <Linkedin color="rgb(0,91,168)" style={{ cursor: "pointer" }} />
-                <Twitter color="rgb(0,91,168)" style={{ cursor: "pointer" }} />
-                <Instagram
-                    color="rgb(0,91,168)"
-                    style={{ cursor: "pointer" }}
-                />
-                <Facebook color="rgb(0,91,168)" style={{ cursor: "pointer" }} />
+                    <div className="nav-container-links">
+                        <Link
+                            className="nav-item"
+                            to="/products"
+                            style={{
+                                paddingBlock: narrowHeader ? "0" : "1rem",
+                                alignItems: narrowHeader
+                                    ? "center"
+                                    : "flex-end",
+                            }}
+                            onMouseOver={() => setHoverProducts(true)}
+                            onMouseLeave={() => setHoverProducts(false)}
+                        >
+                            Ürünler
+                        </Link>
+                        <Link
+                            className="nav-item"
+                            to="/about"
+                            style={{
+                                paddingBlock: narrowHeader ? "0" : "1rem",
+                                alignItems: narrowHeader
+                                    ? "center"
+                                    : "flex-end",
+                            }}
+                        >
+                            Hakkımızda
+                        </Link>
+                        <Link
+                            className="nav-item"
+                            to="/docs"
+                            style={{
+                                paddingBlock: narrowHeader ? "0" : "1rem",
+                                alignItems: narrowHeader
+                                    ? "center"
+                                    : "flex-end",
+                            }}
+                        >
+                            Belgelerimiz
+                        </Link>
+                        <Link
+                            className="nav-item"
+                            to="/references"
+                            style={{
+                                paddingBlock: narrowHeader ? "0" : "1rem",
+                                alignItems: narrowHeader
+                                    ? "center"
+                                    : "flex-end",
+                            }}
+                        >
+                            Referanslar
+                        </Link>
+                        <Link
+                            className="nav-item"
+                            to="/contact"
+                            style={{
+                                paddingBlock: narrowHeader ? "0" : "1rem",
+                                alignItems: narrowHeader
+                                    ? "center"
+                                    : "flex-end",
+                            }}
+                        >
+                            İletişim
+                        </Link>
+                        {/*<div className="socialContainer">
+                            <Linkedin
+                                color="rgb(0,91,168)"
+                                style={{ cursor: "pointer" }}
+                            />
+                            <Twitter
+                                color="rgb(0,91,168)"
+                                style={{ cursor: "pointer" }}
+                            />
+                            <Instagram
+                                color="rgb(0,91,168)"
+                                style={{ cursor: "pointer" }}
+                            />
+                            <Facebook
+                                color="rgb(0,91,168)"
+                                style={{ cursor: "pointer" }}
+                            />
+                        </div>*/}
+                    </div>
+                </div>
             </div>
             {hoverProducts && (
                 <div
