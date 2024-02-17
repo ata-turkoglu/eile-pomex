5b91bc96f4472c46a485c1f71a89a8b8db55ce13
Ata Türkoğlu <ata.turkoglu@reengen.com>
2024-02-17 19:58:03 +0300

    refactor mobile view on metrics

--- src/components/metrics.jsx
+++ src/components/metrics.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import "./metrics.scss";
 
 import { BrickWall, PackageSearch, MapPinned, Settings } from "lucide-react";
 
 function Metrics() {
+    const [mobileView, setMobileView] = useState(false);
+    useEffect(() => {
+        if (window.innerWidth < 768) {
+            setMobileView(true);
+        }
+    }, []);
     return (
         <div className="metrics">
             <div className="metricContainer">
-                <BrickWall size={90} strokeWidth={1} className="metricIcon" />
+                <BrickWall
+                    size={mobileView ? 40 : 90}
+                    strokeWidth={1}
+                    className="metricIcon"
+                />
                 <p className="metricCount">2834</p>
                 <p>Proje Tamamlandı</p>
             </div>
             <div className="metricContainer">
                 <PackageSearch
-                    size={90}
+                    size={mobileView ? 40 : 90}
                     strokeWidth={1}
                     className="metricIcon"
                 />
@@ -21,12 +31,20 @@ function Metrics() {
                 <p>Ürün Çeşidi</p>
             </div>
             <div className="metricContainer">
-                <MapPinned size={90} strokeWidth={1} className="metricIcon" />
+                <MapPinned
+                    size={mobileView ? 40 : 90}
+                    strokeWidth={1}
+                    className="metricIcon"
+                />
                 <p className="metricCount">43</p>
                 <p>Bayilik</p>
             </div>
             <div className="metricContainer">
-                <Settings size={90} strokeWidth={1} className="metricIcon" />
+                <Settings
+                    size={mobileView ? 40 : 90}
+                    strokeWidth={1}
+                    className="metricIcon"
+                />
                 <p className="metricCount">36</p>
                 <p>Hammadde Üretimi</p>
             </div>
