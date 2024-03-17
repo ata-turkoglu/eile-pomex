b96959a0662b7c0a186a1639f0c39d61289099d4
Ata Türkoğlu <ata.turkoglu@reengen.com>
2024-03-17 21:11:09 +0300

    fix a few styles

--- src/components/projectsGroup/projectSamplesSection.jsx
+++ src/components/projectsGroup/projectSamplesSection.jsx
@@ -17,54 +17,54 @@ function ProjectSamplesSection() {
                 <Swiper>
                     <ProjectSampleCard
                         image="assets/projects/project1.jpg"
-                        text="İstanbul Boğaz köprüsü Ortaköy Yaklaşma Viyadüğü Güçlendirmesi"
+                        text="İstanbul Boğaz köprüsü Ortaköy Yaklaşma Viyadüğü Güçlendirmesinde"
                     />
                     <ProjectSampleCard
                         image="assets/projects/project2.jpg"
-                        text="Vestel Manisa Fabrika Binası İnşaatı"
+                        text="Vestel Manisa Fabrika Binası İnşaatında"
                     />
                     <ProjectSampleCard
                         image="assets/projects/project3.jpg"
-                        text=" Antalya Havaalanı Deprem İzolatörü Montajı"
+                        text=" Antalya Havaalanı Deprem İzolatörü Montajında"
                     />
                     <ProjectSampleCard
                         image="assets/projects/project4.jpg"
-                        text="Yeda İnşaat İzmir Atatürk Yüzme Havuzu Tadilatı"
+                        text="Yeda İnşaat İzmir Atatürk Yüzme Havuzu Tadilatında"
                     />
                     <ProjectSampleCard
                         image="assets/projects/project5.jpg"
-                        text="Tüpraş Dolum iskelesi Onarım İşleri"
+                        text="Tüpraş Dolum iskelesi Onarım İşlerinde"
                     />
                     <ProjectSampleCard
                         image="assets/projects/project6.jpg"
-                        text="İzsu İshale Hatları"
+                        text="İzsu İshale Hatlarında"
                     />
                 </Swiper>
             ) : (
                 <div className="content">
                     <ProjectSampleCard
                         image="assets/projects/project1.jpg"
-                        text="İstanbul Boğaz köprüsü Ortaköy Yaklaşma Viyadüğü Güçlendirmesi"
+                        text="İstanbul Boğaz köprüsü Ortaköy Yaklaşma Viyadüğü Güçlendirmesinde"
                     />
                     <ProjectSampleCard
                         image="assets/projects/project2.jpg"
-                        text="Vestel Manisa Fabrika Binası İnşaatı"
+                        text="Vestel Manisa Fabrika Binası İnşaatında"
                     />
                     <ProjectSampleCard
                         image="assets/projects/project3.jpg"
-                        text=" Antalya Havaalanı Deprem İzolatörü Montajı"
+                        text=" Antalya Havaalanı Deprem İzolatörü Montajında"
                     />
                     <ProjectSampleCard
                         image="assets/projects/project4.jpg"
-                        text="Yeda İnşaat İzmir Atatürk Yüzme Havuzu Tadilatı"
+                        text="Yeda İnşaat İzmir Atatürk Yüzme Havuzu Tadilatında"
                     />
                     <ProjectSampleCard
                         image="assets/projects/project5.jpg"
-                        text="Tüpraş Dolum iskelesi Onarım İşleri"
+                        text="Tüpraş Dolum iskelesi Onarım İşlerinde"
                     />
                     <ProjectSampleCard
                         image="assets/projects/project6.jpg"
-                        text="İzsu İshale Hatları"
+                        text="İzsu İshale Hatlarında"
                     />
                 </div>
             )}
