432208e98062406c4b398692b0aa9cd39a68f1e3
Ata Türkoğlu <ata.turkoglu@reengen.com>
2024-05-25 15:04:59 +0300

    fix a bug

--- src/client/components/projectsGroup/projectSampleCard.jsx
+++ src/client/components/projectsGroup/projectSampleCard.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./projectSamplesSection.scss";
 import { useNavigate } from "react-router-dom";
 
 function ProjectSampleCard({ image1, image2, text, icon, product }) {
     const navigate = useNavigate();
+    const [mobile, setMobile] = useState(null);
+    useEffect(() => {
+        window.innerWidth < 768 ? setMobile(true) : setMobile(false);
+    }, []);
     return (
         <div className="groupCard">
             {/*<div className="groupIcon">
@@ -11,10 +15,18 @@ function ProjectSampleCard({ image1, image2, text, icon, product }) {
             </div>*/}
             <div
                 className="imgContainer"
-                onClick={() => navigate("/product-details/" + product)}
+                onClick={() => {
+                    !mobile && navigate("/product-details/" + product);
+                }}
             >
                 <img className="img1" src={image1} />
-                <img className="img2" src={image2} />
+                <img
+                    className="img2"
+                    src={image2}
+                    onClick={() => {
+                        mobile && navigate("/product-details/" + product);
+                    }}
+                />
             </div>
             <div className="textContainer">
                 <p>{text}</p>
