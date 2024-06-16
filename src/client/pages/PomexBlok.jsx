5655b70bffa6cdb376feb1b046f6be48f2097379
Ata Türkoğlu <ata.turkoglu@reengen.com>
2024-06-16 08:31:49 +0300

    refactor blok page

--- src/client/pages/PomexBlok.jsx
+++ src/client/pages/PomexBlok.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./css/pomexBlok.scss";
+import BlokIntro from "../components/blokIntro";
 
 function PomexBlokPage() {
     return (
         <div className="pomex_blok_page">
-            <div className="intro">
+            {/* <div className="intro">
                 <img src="/assets/pomexblok/cure-room.jpg" />
                 <img className="logo" src="assets/logos/pomexblok-logo.png" />
+            </div> */}
+            <div className="intro">
+                <BlokIntro />
             </div>
-            <img src="assets/pomexblok/bims-blok.png" />
+            {/* <img src="assets/pomexblok/bims-blok.png" /> */}
         </div>
     );
 }
